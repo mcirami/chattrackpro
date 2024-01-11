d5852cb3df5c5138a5b69c45839a4788a9ba2ece
Matteo Cirami <mcirami@users.noreply.github.com>
2024-01-11 15:54:37 -0600

    added method in controller to fake clicks

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,10 +6,16 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use App\Click;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 
+	public function index() {
 
+		$clicks = factory(Click::class, 20)->create();
+
+		dd($clicks);
+	}
 }
